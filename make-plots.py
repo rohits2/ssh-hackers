e9f0ce7f442bd643d2595c7f69fd04c2683c3ae2
Rohit Singh <singhrohit2@hotmail.com>
2018-02-09 11:51:15 -0600

    Change make-plots to use local image resources

--- make-plots.py
+++ make-plots.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import gmplot
 import pandas as pd
 
@@ -10,3 +11,10 @@ latitudes, longitudes = zip(*list(set(zip(data["Latitude"], data["Longitude"])))
 gmap.heatmap(heat_latitudes, heat_longitudes, radius=40)
 gmap.scatter(latitudes, longitudes, marker=True, color="#00FF00")
 gmap.draw("failed_logins.html")
+
+
+with open("failed_logins.html", "r") as f:
+    html = f.read()
+html = html.replace("/usr/local/lib/python3.6/dist-packages/gmplot/markers/00FF00.png", "00FF00.png")
+with open("failed_logins.html", "w") as f:
+    f.write(html)
